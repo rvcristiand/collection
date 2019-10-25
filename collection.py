a942c214fdd33f6b9942a2a574c9c95b20b6fbba
rvcristiand <rvcristiand@gmail.com>
2019-10-24 19:21:52 -0500

    Second version: metaclass

--- collection.py
+++ collection.py
@@ -1,51 +1,64 @@
-class UniqueInstances:
-    __slots__ = []
-    instancesAttrs = set()
-
-    def __new__(cls, *args):
-        if (len(args) == len(cls.__slots__)) and args not in cls.instancesAttrs:
-            cls.instancesAttrs.add(args)
-
-            return super(UniqueInstances, cls).__new__(cls)
-        else:
-            print("Warning: " +
-                  "There is another instance of the class " +
-                  "'{}'".format(cls.__name__) +
-                  " with the same attibutes. The object was not created.")
-
-            return None
-
-    def __init__(self, *args):
+class UniqueInstances(type):
+    def init(self, *args):
         for key, value in zip(self.__slots__, args):
             setattr(self, key, value)
 
-    def __setattr__(self, key, value):
+    def setattr(self, key, value):
         if hasattr(self, key):
-            instanceAttrs = tuple((getattr(self, _key) if _key != key else value
-                                  for _key in self.__slots__))
-            if instanceAttrs not in self.__class__.instancesAttrs:
-                self.__class__.instancesAttrs.remove(self.instanceAttrs)
-                self.__class__.instancesAttrs.add(instanceAttrs)
+            _instanceAttr = tuple((getattr(self, _key)
+                                   if _key != key else value
+                                   for _key in self.__slots__))
+
+            if _instanceAttr not in self.__class__.instancesAttrs:
+                self.__class__.instancesAttrs.\
+                    remove(self.instanceAttrs)
+                self.__class__.instancesAttrs.add(_instanceAttr)
             else:
                 print("Warning: " +
                       "There is another instance of the class " +
                       "'{}'".format(self.__class__.__name__) +
                       " with the same attibutes. The object was not changed.")
 
-        super(UniqueInstances, self).__setattr__(key, value)
+                return None
 
-    @property
-    def instanceAttrs(self):
+        object.__setattr__(self, key, value)
+
+    def get_instancesAttr(self):
         return tuple(getattr(self, key) for key in self.__slots__)
 
-    def __repr__(self):
+    def repr(self):
         return "{}{}".format(self.__class__.__name__, str(self.instanceAttrs))
 
-    def __del__(self):
+    def delete(self):
         self.__class__.instancesAttrs.remove(self.instanceAttrs)
 
-class Coordinate(UniqueInstances):
-    __slots__ = ['x', 'y', 'z']
+    def __new__(cls, name, bases, dict):
+        dict['instancesAttrs'] = set()
+        dict['__init__'] = UniqueInstances.init
+        dict['__setattr__'] = UniqueInstances.setattr
+        dict['instanceAttrs'] = property(UniqueInstances.get_instancesAttr)
+        dict['__repr__'] = UniqueInstances.repr
+        dict['__del__'] = UniqueInstances.delete
+
+        return super().__new__(cls, name, bases, dict)
+
+    def __call__(cls, *args):
+        assert len(args) == len(cls.__slots__)
+
+        if args not in cls.instancesAttrs:
+            cls.instancesAttrs.add(args)
+
+            return super().__call__(*args)
+        else:
+            print("Warning: " +
+                  "There is another instance of the class " +
+                  "'{}' ".format(cls.__name__) +
+                  "with the same attributes. The object was not created.")
+
+            return None
+
+class Coordinate(metaclass=UniqueInstances):
+    __slots__ = ('x', 'y', 'z')
 
     def __init__(self, x, y, z):
         super(Coordinate, self).__init__(x, y, z)
@@ -53,94 +66,58 @@ class Coordinate(UniqueInstances):
 
 if __name__ == "__main__":
     # view instances' attrs
-    print(Coordinate.instancesAttrs)
+    print(Coordinate.instancesAttrs)  # set()
 
     # instantiates first object
     coordinate1 = Coordinate(0, 0, 0)
-    print(coordinate1)
-
+    print(coordinate1)  # Coordinate(0, 0, 0)
+    
     # try to add a attr to first object
     try:
         coordinate1.a = 1
     except Exception as e:
-        print(e)
-
+        print(e)  # 'Coordinate' object has no attribute 'a'
+    
     # view instances' attrs
     print(Coordinate.instancesAttrs)
-
+    
     # try instantiates second object with attrs first object
-    coordinate2 = eval(repr(coordinate1))
-    print(coordinate2)
-
+    coordinate2 = eval(repr(coordinate1))  # Warning: There is another instance of the class 'Coordinate' with the same attibutes. The object was not created.
+    print(coordinate2)  # None
+    
     # view instances' attrs
     print(Coordinate.instancesAttrs)
-
+    
     # del first object
     del(coordinate1)
-
+    
     # view instances' attrs
-    print(Coordinate.instancesAttrs)
-
+    print(Coordinate.instancesAttrs)  # set()
+    
     # create second object
     coordinate2 = Coordinate(0, 0, 0)
+    coordinate3 = Coordinate(1, 0, 0)
     print(coordinate2)
-
+    
     # view instances' attrs
-    print(Coordinate.instancesAttrs)
-
+    print(Coordinate.instancesAttrs)  # {(1, 0, 0), (0, 0, 0)}
+    
     # change value attrs
     coordinate2.x = 1
     print(coordinate2)
-
+    
     # view instances' attrs
     print(Coordinate.instancesAttrs)
-
-    # first object
-    # myObj1 = UniqueInstances(1, 2, number1=1, number2=2)
-    # print(myObj1)
-
-    # try second object with the same
-    # myObj2 = eval(repr(myObj1))
-    # print(myObj2)
-
-    # del object
-    # myObj1 = None
-
-    # create object
-    # myObj2 = UniqueInstances(1, 2, number1=1, number2=2)
-    # print(myObj2)
-
-    # print(myObj2.__dict__.keys())
-
-
-
-
-
-    # myList = []
-    # numbers = [i for i in range(20)]
-    #
-    # for i in range(10):
-    #     number = random.choice(numbers)
-    #     myList.append(UniqueInstances(number, number,
-    #                                   number1=number, number2=number))
-    #
-    # print(myList)
-    # for item in myList:
-    #     print(item)
-    # mySet = set()
-    # myDict = {'a':1, 'b':2, 'c':3}
-    # mySet.add(tuple(myDict.items()))
-    # print(type(myDict.items()))
-
-
-
-    # myList = []
-    #
-    # myList.append(Coordinate(0, 0, 0))
-    #
-    # print(myList[0])
-    #
-    # for i in range(10):
-    #     myList.append(Coordinate(i, i, i))
-    #
-    # print(', '.join((str(obj) for obj in myList)))
+    
+    # create third object
+    print(coordinate3)
+    
+    # view instances' attrs
+    print(Coordinate.instancesAttrs)
+    
+    # change value attrs
+    coordinate3.x = 0  # Warning: There is another instance of the class 'Coordinate' with the same attibutes. The object was not changed.
+    print(coordinate3)  # Coordinate(1, 0, 0)
+    
+    print(UniqueInstances.instancesAttrs)  # {(1, 0, 0), (0, 0, 0)}
+    print(Coordinate.instancesAttrs)  # {(1, 0, 0), (0, 0, 0)}
